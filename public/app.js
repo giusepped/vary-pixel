2a4809e02989aae1c938d995cb480b0efcb93457
giuseppe de santis <giusdesan@gmail.com>
2015-09-21 19:21:24 +0100

    refactored classes and got first test passing

--- public/app.js
+++ public/app.js
@@ -1,19 +1,14 @@
+
 $(document).ready(function() {
   var board = $(".board")[0];
-  var boardSize = 1000;
-  var pixelSize = 10;
 
-  var ctx = board.getContext("2d");
+  board.height = board.width = 1000;
+  var boardInterface = new BoardInterface(board.getContext("2d"));
 
-  board.height = boardSize;
-  board.width = boardSize;
+  $(board).click(drawOn);
 
-
-  $(board).click(function(event) {
-    mouseX = event.pageX - (pixelSize + 5);
-    mouseY = event.pageY - (pixelSize);
-    ctx.fillStyle = "black";
-    ctx.fillRect(mouseX, mouseY, pixelSize, pixelSize);
-  });
+  function drawOn(event) {
+    boardInterface.createPixel(event.x, event.y);
+  }
 
 });
---
+++ spec/jasmine-jquery/boardInterfaceSpec.js
@@ -0,0 +1,17 @@
+describe("BoardInterface", function() {
+
+  var bI;
+  var ctx = {
+    fillRect: function() {}
+  };
+
+  it("creates a pixel at a given location on the board", function() {
+    spyOn(ctx, "fillRect");
+    bI = new BoardInterface(ctx)
+
+    bI.createPixel(100, 100, 10);
+
+    expect(ctx.fillRect).toHaveBeenCalledWith(85, 90, 10, 10);
+  })
+
+})
