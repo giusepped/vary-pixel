292e6b698422e37961af31cf94f10f0abf6d0b2f
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-09-22 14:28:23 +0100

    added color

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
   var board = $(".board")[0];
   var ctx = board.getContext("2d");
@@ -6,17 +5,24 @@ $(document).ready(function() {
   var boardSize = 1000;
   var pixelSize = 15;
   var background = $('.grid')[0];
-  var gridContext = background.getContext('2d')
+  var gridContext = background.getContext('2d');
+  var myColor;
 
   board.height = board.width = boardSize;
   background.height = background.width = boardSize;
   var opts = { distance: pixelSize};
   new Grid(opts).draw(gridContext);
 
+  $(".color").on("change", function() {
+    myColor = $(".color").val()
+    console.log(myColor)
+  });
+
+
   $(board).click(drawOn);
 
   function drawOn() {
-    boardInterface.createPixel(event.pageX, event.pageY, pixelSize);
+    boardInterface.createPixel(event.pageX, event.pageY, pixelSize, myColor);
   }
 
   $('.toggle-grid').click(function () {
--- public/js/pixel.js
+++ public/js/pixel.js
@@ -1,23 +1,17 @@
-<<<<<<< HEAD:public/pixel.js
-=======
 function BoardInterface(context) {
   this.context = context;
 }
 
-BoardInterface.prototype.createPixel = function(x, y, size) {
+BoardInterface.prototype.createPixel = function(x, y, size, myColor) {
   x = Math.floor(x / size) * size;
   y = Math.floor(y / size) * size;
-  PixelGenerator.createDot(this.context, x, y, size);
+  PixelGenerator.createDot(this.context, x, y, size, myColor);
 };
->>>>>>> 66c0221b5034e1ddd6d3b0ac67b4fbfe77675261:public/js/pixel.js
 
 var PixelGenerator = (function() {
-  // $(".color").on("change", function() {
-  //   console.log($(".color").val())
-  // });
 
-  function createDot(ctx, x, y, size) {
-    ctx.fillStyle = "black";
+  function createDot(ctx, x, y, size, myColor) {
+    ctx.fillStyle = "#" + myColor;
     ctx.fillRect(x, y, size, size);
   };
 
@@ -25,29 +19,3 @@ var PixelGenerator = (function() {
     createDot: createDot
   }
 })();
-<<<<<<< HEAD:public/pixel.js
-
-function BoardInterface(context) {
-  this.context = context
-}
-
-BoardInterface.prototype.createPixel = function(x, y, size) {
-  // here transform x and y as you want
-  PixelGenerator.createDot(this.context, x, y, size)
-};
-
-
-  // var rgbString = "rgb(0, 70, 255)"; // get this in whatever way.
-
-  // var parts = rgbString.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-  // // parts now should be ["rgb(0, 70, 255", "0", "70", "255"]
-
-  // delete (parts[0]);
-  // for (var i = 1; i <= 3; ++i) {
-  //   parts[i] = parseInt(parts[i]).toString(16);
-  //   if (parts[i].length == 1) parts[i] = '0' + parts[i];
-  // } 
-  // var hexString ='#'+parts.join('').toUpperCase(); // "#0070FF"
-  // console.log(hexString)
-=======
->>>>>>> 66c0221b5034e1ddd6d3b0ac67b4fbfe77675261:public/js/pixel.js
