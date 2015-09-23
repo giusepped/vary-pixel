a3a88a65581f00a95b489ecb2c1f71abf4d6af16
Aftermaths <fnzzhou@gmail.com>
2015-09-23 09:24:58 +0100

    cannot fill same space twice

--- public/js/pixel.js
+++ public/js/pixel.js
@@ -1,11 +1,23 @@
 function BoardInterface(context) {
   this.context = context;
+  this.userLimit = 100;
 }
 
-BoardInterface.prototype.createPixel = function(x, y, size, pixelColor) {
+BoardInterface.prototype.createPixel = function(x, y, size, myColor) {
+
+  var a = this.context.getImageData(x, y, 1, 1).data;
+  var rgb = a[0] + '' + a[1] + '' + a[2] + '' + a[3];
+
   x = Math.floor(x / size) * size;
   y = Math.floor(y / size) * size;
-  PixelGenerator.createDot(this.context, x, y, size, pixelColor);
+
+  if (rgb == 0000) {
+    PixelGenerator.createDot(this.context, x, y, size, myColor);
+    this.userLimit--;
+  } else {
+    this.context.clearRect(x, y, size, size);
+    this.userLimit++;
+  };
 };
 
 var PixelGenerator = (function() {
--- spec/jasmine-jquery/boardInterfaceSpec.js
+++ spec/jasmine-jquery/boardInterfaceSpec.js
@@ -2,7 +2,8 @@ describe("BoardInterface", function() {
 
   var bI;
   var ctx = {
-    fillRect: function() {}
+    fillRect: function() {},
+    getImageData: function() {}
   };
 
   it("creates a pixel at a given location on the board", function() {
@@ -13,6 +14,29 @@ describe("BoardInterface", function() {
     coordinate = Math.floor(100 / 15) * 15
 
     expect(ctx.fillRect).toHaveBeenCalledWith(coordinate, coordinate, 15, 15);
+  });
+
+
+  it("can create a pixel with different colors", function() {
+
+  });
+
+  it('cannot fill the same pixel twice', function() {
+
+  });
+
+  it('decreases the user limit count after painting', function() {
+    setFixtures("<canvas id='screen' style='width:1000px; height:1000px;'></canvas>")
+    var canvas = $('#canvas');
+    var ctx = canvas.getContext('2d');
+    console.log(canvas);
+
+    bI.createPixel(100, 100, 15);
+    expect(bI.userLimit).toEqual(99);
+  });
+
+  it('knows to increase user limit count after removing', function() {
+
   });
 
 });
