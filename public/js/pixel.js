afe2198b33e2b34e79a42c12b88d7ad032e0967b
Aftermaths <fnzzhou@gmail.com>
2015-09-23 11:06:00 +0100

    test passing and refactored

--- public/js/pixel.js
+++ public/js/pixel.js
@@ -5,29 +5,44 @@ function BoardInterface(context) {
 
 BoardInterface.prototype.createPixel = function(x, y, size, myColor) {
 
-  var a = this.context.getImageData(x, y, 1, 1).data;
-  var rgb = a[0] + '' + a[1] + '' + a[2] + '' + a[3];
-
   x = Math.floor(x / size) * size;
   y = Math.floor(y / size) * size;
 
-  if (rgb == 0000) {
-    PixelGenerator.createDot(this.context, x, y, size, myColor);
+  if (PixelGenerator.createDot(this.context, x, y, size, myColor)) {
     this.userLimit--;
+    console.log(this.userLimit);
   } else {
-    this.context.clearRect(x, y, size, size);
     this.userLimit++;
+    console.log(this.userLimit);
   };
 };
 
 var PixelGenerator = (function() {
 
   function createDot(ctx, x, y, size, pixelColor) {
-    ctx.fillStyle = "#" + pixelColor;
-    ctx.fillRect(x, y, size, size);
+    if (WhatColour.pickColour(ctx, x, y) === '0000') {
+      ctx.fillStyle = "#" + pixelColor;
+      ctx.fillRect(x, y, size, size);
+      return true
+    } else {
+      ctx.clearRect(x, y, size, size);
+      return false
+    }
   };
 
   return {
     createDot: createDot
+  }
+})();
+
+var WhatColour = (function() {
+  function pickColour(ctx, x, y) {
+    var a = ctx.getImageData(x, y, 1, 1).data;
+    var rgb = a[0] + '' + a[1] + '' + a[2] + '' + a[3];
+    return rgb
+  }
+
+  return {
+    pickColour: pickColour
   }
 })();
--- spec/jasmine-jquery/boardInterfaceSpec.js
+++ spec/jasmine-jquery/boardInterfaceSpec.js
@@ -3,15 +3,21 @@ describe("BoardInterface", function() {
   var bI;
   var ctx = {
     fillRect: function() {},
+    getImageData: function() {}
   };
+
+  beforeEach(function() {
+    bI = new BoardInterface(ctx)
+  })
 
   it("creates a pixel at a given location on the board", function() {
     spyOn(ctx, "fillRect");
+    spyOn(ctx, "getImageData").and.returnValue({data: [0,0,0,0]});
     bI = new BoardInterface(ctx)
 
     bI.createPixel(100, 100, 15);
     coordinate = Math.floor(100 / 15) * 15
-
+    expect(ctx.getImageData).toHaveBeenCalledWith(coordinate, coordinate, 1, 1)
     expect(ctx.fillRect).toHaveBeenCalledWith(coordinate, coordinate, 15, 15);
   });
 
@@ -20,22 +26,23 @@ describe("BoardInterface", function() {
   //
   // });
   //
-  // it('cannot fill the same pixel twice', function() {
-  //
-  // });
-  //
-  // it('decreases the user limit count after painting', function() {
-  //   setFixtures("<canvas id='screen' style='width:1000px; height:1000px;'></canvas>")
-  //   var canvas = $('#canvas');
-  //   var ctx = canvas.getContext('2d');
-  //   console.log(canvas);
+
+  it('decreases the user limit count after painting', function() {
+    spyOn(ctx, "getImageData").and.returnValue({data: [0,0,0,0]});
+
+    bI.createPixel(100, 100, 15);
+    expect(bI.userLimit).toEqual(99);
+  });
+
+  // cannot double spy
+  // it('knows to increase user limit count after removing', function() {
+  //   spyOn(ctx, "getImageData").and.returnValue({data: [0,0,0,255]});
   //
   //   bI.createPixel(100, 100, 15);
   //   expect(bI.userLimit).toEqual(99);
-  // });
-  //
-  // it('knows to increase user limit count after removing', function() {
-  //
+  //   // ctx.getImageData.data = [0,0,0,255];
+  //   bI.createPixel(100, 100, 15);
+  //   expect(bI.userLimit).toEqual(100);
   // });
 
 });
