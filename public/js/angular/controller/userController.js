2297da946ee8ab2759d8354858c6e9a625082f84
Rebecca Appleyard <rklappleyard@gmail.com>
2015-09-24 13:25:31 +0100

    Sign in implemented

--- public/js/angular/controller/userController.js
+++ public/js/angular/controller/userController.js
@@ -10,10 +10,21 @@ homepage.controller('UserController', ['$scope', function($scope) {
       success:function(user){
         console.log("I just saved a user");
       },
-      error:function(user, error){
+      error:function(user){
         console.log("didn't work");
       }
     })
+  }
+
+  $scope.signInUser = function(username, password) {
+    Parse.User.logIn(username, password, {
+      success: function(user) {
+        console.log('I have signed in');
+      },
+      error: function(user) {
+        console.log('error');
+      }
+    });
   }
 
 }]);
