3f667d705fe6ec0402ce02de4eac3379e1bb6331
Rebecca Appleyard <rklappleyard@gmail.com>
2015-09-25 11:28:26 +0100

    Log in form displaying

--- spec/e2e/homepage.features.js
+++ spec/e2e/homepage.features.js
@@ -5,37 +5,47 @@ describe('homepage', function() {
   });
 
   var board = element.all(by.repeater('board in boards'));
-  var addBoardBtn = element(by.className('addBoardBtn'));
-  var boardTitle = element(by.model('boardDesc'));
-  var newDesc = element(by.model('newDescValue'));
-  var newDescBtn = element(by.className('newDescSubmit'));
-  var editBtn = element(by.className('newDescBtn'));
-  var delBtn = element(by.className('boardDelBtn'));
+  var signInBtn = element(by.className('logInButton'));
+  var signUpBtn = element(by.className('signUpButton'));
+  var logOutBtn = element(by.className('logOutButton'));
+
+  // var addBoardBtn = element(by.className('addBoardBtn'));
+  // var boardTitle = element(by.model('boardDesc'));
+  // var newDesc = element(by.model('newDescValue'));
+  // var newDescBtn = element(by.className('newDescSubmit'));
+  // var editBtn = element(by.className('newDescBtn'));
+  // var delBtn = element(by.className('boardDelBtn'));
 
   it('should have a title', function() {
     expect(browser.getTitle()).toEqual('Vary Pixel');
   });
 
-  xit('should have list of all boards', function() {
-    boardTitle.sendKeys('test');
-    addBoardBtn.click();
-    expect(board.get(0).getText()).toContain('test');
+  describe('user registration', function() {
+    it('should have a log in feature', function() {
+      element(by.className('logInButton')).click();
+      expect(element(by.className('sign-in')).isDisplayed()).toBe(true);
+    });
   });
+  // xit('should have list of all boards', function() {
+  //   boardTitle.sendKeys('test');
+  //   addBoardBtn.click();
+  //   expect(board.get(0).getText()).toContain('test');
+  // });
 
-  xit('edit a board desc', function () {
-    boardTitle.sendKeys('test');
-    addBoardBtn.click();
-    editBtn.click();
-    newDesc.sendKeys('test2');
-    newDescBtn.click();
-    expect(board.get(0).getText()).toContain('test2')
-  });
+  // xit('edit a board desc', function () {
+  //   boardTitle.sendKeys('test');
+  //   addBoardBtn.click();
+  //   editBtn.click();
+  //   newDesc.sendKeys('test2');
+  //   newDescBtn.click();
+  //   expect(board.get(0).getText()).toContain('test2')
+  // });
 
-  xit('can delete a board', function () {
-    boardTitle.sendKeys('test');
-    addBoardBtn.click();
-    editBtn.click();
-    delBtn.click();
-    expect(board.length).toBeUndefined();
-  });
+  // xit('can delete a board', function () {
+  //   boardTitle.sendKeys('test');
+  //   addBoardBtn.click();
+  //   editBtn.click();
+  //   delBtn.click();
+  //   expect(board.length).toBeUndefined();
+  // });
 });
