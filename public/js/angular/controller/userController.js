f831ae559ebad8fa938dad71b8edb6f7a71c990c
Rebecca Appleyard <rklappleyard@gmail.com>
2015-09-25 15:12:04 +0100

    Username now appearing correctly - needs styling

--- public/js/angular/controller/userController.js
+++ public/js/angular/controller/userController.js
@@ -1,38 +1,48 @@
 homepage.controller('UserController', ['$scope', function($scope) {
+
+  var self = this;
+  $scope.username = '';
+
   $scope.saveUser = function(username, email, password) {
     var user = new Parse.User();
     user.set("username", username);
     user.set("password", password);
     user.set("email", email);
-
     user.signUp(null, {
       success:function(user){
-        console.log("I just saved a user");
+        $scope.username = user.get("username");
+        console.log($scope.username);
       },
       error:function(user){
         console.log("didn't work");
       }
-    })
+    }).then(function(user) {
+      $scope.username = user.get("username");
+      $scope.$apply();
+    });
   }
 
   $scope.signInUser = function(username, password) {
     Parse.User.logIn(username, password, {
       success: function(user) {
-        console.log('I have signed in');
+        console.log('success');
       },
       error: function(user) {
         console.log('error');
       }
+    }).then(function(user) {
+      $scope.username = user.get("username");
+      $scope.$apply();
     });
   }
 
   $scope.signOutUser = function(username, password) {
     Parse.User.logOut();
-    console.log('I have signed out');
+    $scope.username = "Logged out";
+    console.log($scope.username);
   }
 
-  $scope.currentUser = function() {
-    $scope.loggedIn = Parse.User.current() ? true : false
-    console.log($scope.loggedIn);
+  $scope.currentUser = {
+    name: $scope.username
   }
 }]);
