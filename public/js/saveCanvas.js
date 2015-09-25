17c5079460621f772b305bbaee48547388290760
giuseppe de santis <giusdesan@gmail.com>
2015-09-25 18:58:02 +0100

    finished association, cannot edit canvases unless logged in

--- public/js/saveCanvas.js
+++ public/js/saveCanvas.js
@@ -9,9 +9,11 @@ function hideFlashMessage(message) {
 function updateCanvas(board, imgID) {
   var canvasData = board.toDataURL('image/png');
   var query = new Parse.Query(canvases);
+  var currentCanvas;
   var file = new Parse.File("canvasData.txt", { base64: canvasData });
   query.get(imgID, {
     success:function(currentCanvas) {
+      currentCanvas = currentCanvas;
       currentCanvas.set("picture", file);
       currentCanvas.save();
       var message = $('.save-alert').text('Your drawing has been updated!');
@@ -19,9 +21,33 @@ function updateCanvas(board, imgID) {
       var currentUser = Parse.User.current();
       var contributors = currentCanvas.relation("contributors");
       contributors.add(currentUser);
+      contributors.query().find({
+        success:function(result){
+          console.log(result);
+        },
+        error:function(){
+          console.log("oops");
+        }
+      });
     },
     error: function(currentCanvas) {
       console.log("Could not find the canvas");
     }
   });
+
+  // var query = new Parse.Query(canvases);
+  // query.get(imgID, {
+  //   success:function(canvas) {
+  //     var relation = canvas.relation("contributors");
+  //     relation.query().find({
+  //       success:function(result){
+  //         console.log(result);
+  //       }, error:function(){
+  //         console.log("something went wrong");
+  //       }
+  //     })
+  //   }, error:function(){
+  //     console.log("something went wrong 2");
+  //   }
+  // })
 }
