2dd55815c5b5a376e36df0916c5ee766b8e0fab1
Aftermaths <fnzzhou@gmail.com>
2015-09-28 13:31:19 +0100

    search bar works

--- public/js/angular/controller/homepageController.js
+++ public/js/angular/controller/homepageController.js
@@ -18,26 +18,6 @@ homepage.controller('HomeController', ['$scope', '$q', 'AllCanvas', '$rootScope'
     }
   }
 
-  function getBoard() {
-    var deferred = $q.defer();
-    var query = new Parse.Query(canvases);
-
-    query.find({
-      success: function(results) {
-        deferred.resolve(results);
-      },
-      error: function(error) {
-        deferred.reject(error.message);
-      }
-    });
-    return deferred.promise;
-  }
-
-  getBoard().then(function(boards) {
-    AllCanvas.setBoard(boards);
-    $scope.boards = boards;
-  });
-
   function saveToParse(object, description) {
     object.save({
       picture: null,
@@ -62,4 +42,50 @@ homepage.controller('HomeController', ['$scope', '$q', 'AllCanvas', '$rootScope'
   $scope.setCurrent = function(id) {
     AllCanvas.setCurrent(id);
   }
+  
+  $scope.fetchPopular = function() {
+    function getBoard() {
+      var deferred = $q.defer();
+      var query = new Parse.Query(canvases);
+
+      query.find({
+        success: function(results) {
+          deferred.resolve(results);
+        },
+        error: function(error) {
+          deferred.reject(error.message);
+        }
+      });
+      return deferred.promise;
+    }
+
+    getBoard().then(function(boards) {
+      AllCanvas.setBoard(boards);
+      $scope.boards = boards;
+    });
+  }
+
+  $scope.search = function (description) {
+    function getBoard() {
+      var deferred = $q.defer();
+      var query = new Parse.Query(canvases);
+      query.startsWith("description", description)
+      query.find({
+        success: function(results) {
+          deferred.resolve(results);
+        },
+        error: function(error) {
+          deferred.reject(error.message);
+        }
+      });
+      return deferred.promise;
+    }
+
+    getBoard().then(function(boards) {
+      AllCanvas.setBoard(boards);
+      $scope.boards = boards;
+    });
+  }
+
+  $scope.fetchPopular();
 }]);
