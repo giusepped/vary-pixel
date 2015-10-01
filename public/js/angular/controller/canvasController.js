a1d2c92270303932dde18ed4bfd8f4ff97b7b200
Aftermaths <fnzzhou@gmail.com>
2015-10-01 10:32:27 +0100

    refactored

--- public/js/angular/controller/canvasController.js
+++ public/js/angular/controller/canvasController.js
@@ -1,38 +1,12 @@
-homepage.controller('CanvasController', ['$scope', 'CanvasProvider', '$timeout', '$interval', '$q', function($scope, CanvasProvider, $timeout, $interval, $q) {
-  var socket = io();
-  var board = $(".board")[0];
-  var boardCtx = board.getContext("2d");
-  var boardSize = 1500;
-  var pixelSize = boardSize / 100;
-  var boardInterface = new BoardInterface(boardCtx);
-  var background = $('.grid')[0];
-  var gridContext = background.getContext('2d');
-  var paletteCanvas = $('.colour-palette')[0];
-  var paletteCtx = paletteCanvas.getContext('2d');
-  var pixelColor = 'rgba(0,0,0,255)';
-  var opts = {
-    distance: pixelSize
-  };
-  var chosenCanvas = new Image();
-  var colourPaletteImg = new Image();
-  var userObj = Parse.User.current();
-  var username = userObj.get("username");
+homepage.controller('CanvasController', ['$scope', 'CanvasProvider', 'PixelFactory', '$timeout', '$interval', '$q', function($scope, CanvasProvider, PixelFactory, $timeout, $interval, $q) {
 
   $scope.userAction = 'draw';
-
-  board.height = board.width = boardSize;
-  background.height = background.width = boardSize;
-  new Grid(opts).draw(gridContext);
-
-  function imgID() {
-    return CanvasProvider.getCurrent()[0];
-  }
 
   $scope.imgDesc = function() {
     return CanvasProvider.getCurrent()[1];
   }
 
-  $scope.action = function (action) {
+  $scope.action = function(action) {
     action = action || 'draw';
     if ($scope.userAction === 'erase') {
       $scope.userAction = 'draw';
@@ -41,18 +15,92 @@ homepage.controller('CanvasController', ['$scope', 'CanvasProvider', '$timeout',
     };
   }
 
+  var socket = io();
+  var boardSize = 1500;
+  var pixelSize = boardSize / 100;
+
+  var board = $(".board")[0];
+  var boardCtx = board.getContext("2d");
+
+  var background = $('.grid')[0];
+  var gridContext = background.getContext('2d');
+
+  var paletteCanvas = $('.colour-palette')[0];
+  var paletteCtx = paletteCanvas.getContext('2d');
+  var pixelColor = 'rgba(0,0,0,255)';
+
+  var opts = {
+    distance: pixelSize
+  };
+
+  var chosenCanvas = new Image();
+  var colourPaletteImg = new Image();
+
+  var userObj = Parse.User.current();
+  var username = userObj.get("username");
+
+  board.height = board.width = boardSize;
+  background.height = background.width = boardSize;
+  paletteCanvas.width = paletteCanvas.height = 300;
+  new Grid(opts).draw(gridContext);
+
+  $('.colour-palette').hide();
+
+  CanvasProvider.searchBy('objectId', imgID()).then(function(result) {
+    chosenCanvas.src = result[0].attributes.Base64;
+  })
+  colourPaletteImg.src = 'images/ColorWheel-Base.png';
+
+  socket.on('chat message', appendMessage);
+
+  socket.on('coordinates', function(data) {
+    PixelFactory.createPixel(boardCtx, data[0], data[1], data[2], pixelSize, data[3]);
+  });
+
+  chosenCanvas.onload = function() {
+    joinRoom();
+    boardCtx.drawImage(chosenCanvas, 0, 0, boardSize, boardSize);
+  }
+
+  colourPaletteImg.onload = function() {
+    paletteCtx.drawImage(colourPaletteImg, 0, 0, paletteCanvas.width, paletteCanvas.height);
+  }
+  window.onunload = function() {
+    leaveRoom();
+  }
+
+  function imgID() {
+    return CanvasProvider.getCurrent()[0];
+  }
+
   function drawOn(action, x, y, colour) {
     action = $scope.userAction;
     x = x || event.offsetX;
     y = y || event.offsetY;
     colour = colour || pixelColor;
-    boardInterface.createPixel(action, x, y, pixelSize, colour);
+    PixelFactory.createPixel(boardCtx, action, x, y, pixelSize, colour);
     socket.emit('coordinates', [action, x, y, colour, imgID()]);
   };
 
-  socket.on('coordinates', function(data) {
-    boardInterface.createPixel(data[0], data[1], data[2], pixelSize, data[3]);
-  });
+  function appendMessage(msg) {
+    $('.messages').append($('<li>').text(msg));
+  }
+
+  function joinRoom() {
+    socket.emit('joinRoom', [imgID(), username]);
+    socket.on('unsaved coordinates', function(data) {
+      for (var i = 0; i < data.length; i++) {
+        PixelFactory.createPixel(boardCtx, data[i][0], data[i][1], data[i][2], pixelSize, data[i][3]);
+      }
+    })
+  }
+
+  function leaveRoom() {
+    socket.emit('leaveRoom', [imgID(), username]);
+    socket.removeListener('chat message', appendMessage);
+    CanvasProvider.updateCanvas(board, imgID())
+    CanvasProvider.setCurrent(null);
+  }
 
   $(board).mousedown(function() {
     $('.colour-palette').fadeOut('slow');
@@ -87,38 +135,14 @@ homepage.controller('CanvasController', ['$scope', 'CanvasProvider', '$timeout',
   $('.colour-palette').click(function() {
     var x = event.offsetX;
     var y = event.offsetY;
-    pixelColor = WhatColour.pickColour(paletteCtx, x, y);
+    pixelColor = PixelFactory.whatColour(paletteCtx, x, y);
     $('.colour-palette').fadeToggle('slow');
     $('.colour-palette-toggle').fadeToggle('slow');
   })
 
-  chosenCanvas.onload = function() {
-    joinRoom();
-    boardCtx.drawImage(chosenCanvas, 0, 0, boardSize, boardSize);
-  }
-
-  colourPaletteImg.onload = function() {
-    paletteCanvas.width = paletteCanvas.height = 300;
-    paletteCtx.drawImage(colourPaletteImg, 0, 0, paletteCanvas.width, paletteCanvas.height);
-  }
-
   $('.home-button').click(function() {
     leaveRoom();
   })
-
-  window.onunload = function() {
-    leaveRoom();
-  }
-
-  $('.colour-palette').hide();
-
-  CanvasProvider.searchBy('objectId', imgID()).then(function(result) {
-    chosenCanvas.src = result[0].attributes.Base64;
-  })
-
-
-
-  colourPaletteImg.src = 'images/ColorWheel-Base.png';
 
   $('.chat-button').click(function() {
     $('.chatbox').toggle();
@@ -129,27 +153,5 @@ homepage.controller('CanvasController', ['$scope', 'CanvasProvider', '$timeout',
     $('.msg').val('');
     return false;
   });
-
-  socket.on('chat message', appendMessage);
-
-  function appendMessage(msg) {
-    $('.messages').append($('<li>').text(msg));
-  }
-
-  function joinRoom() {
-    socket.emit('joinRoom', [imgID(), username]);
-    socket.on('unsaved coordinates', function(data) {
-      for(var i = 0; i < data.length; i++) {
-        boardInterface.createPixel(data[i][0], data[i][1], data[i][2], pixelSize, data[i][3]);
-      }
-    })
-  }
-
-  function leaveRoom(){
-    socket.emit('leaveRoom', [imgID(), username]);
-    socket.removeListener('chat message', appendMessage);
-    CanvasProvider.updateCanvas(board, imgID())
-    CanvasProvider.setCurrent(null);
-  }
 
 }])
